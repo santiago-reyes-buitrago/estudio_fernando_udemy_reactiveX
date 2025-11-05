5c2cc5a5c448868a192f6a4ed5146e4d8d905c06
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-11-05 17:38:43 -0500

    terminada seccion 11 - reforzamiento

---
+++ src/exercises/03-randoms.ts
@@ -0,0 +1,27 @@
+import {interval, combineLatest, Subject, tap} from 'rxjs';
+import {take, map} from 'rxjs/operators';
+/**
+ * Ejercicio: Realizar que los dos observables finales,
+ * emitan exactamente el mismo valor
+ *
+ * Tip: Hot Observable? subjects?
+ */
+
+(() => {
+
+    // == NO TOCAR este bloque ====================
+    const reloj$ = interval(1000).pipe(
+        take(5),
+        map(val => Math.round(Math.random() * 100))
+    );
+    // No tocar la creación del observable
+    // ============================================
+    const reloj2$ = new Subject<number>()
+
+
+    // Estos dos observables deben de emitir exactamente los mismos valores
+    reloj$.pipe(tap(val => reloj2$.next(val))).subscribe(val => console.log('obs1', val));
+    reloj2$.subscribe(val => console.log('obs2', val));
+
+
+})();
